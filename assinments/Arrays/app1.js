5830a7e69d20a7aee3da1f32b34757f4019244c4
Harikrishna7293 <102645517+Harikrishna7293@users.noreply.github.com>
2022-04-28 10:20:31 +0530

    Add files via upload

---
+++ assinments/Arrays/app1.js
@@ -0,0 +1,6 @@
+//functions of arrays
+//shift() ->remove the first element of Array
+
+console.log(`----------shift()----------`);
+let technologies = [`html`,`css`,`javascript`,`jquery`,`bootstrap`];
+console.log(technologies);
